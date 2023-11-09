134659e2253b7fce798f755e933a953e9fbe78d1
CRHernandez14 <chrisrhernandezp@gmail.com>
2023-11-09 10:38:25 -0600

    Configuracion de sesiones

--- src/app/components/clientes/login/login.component.ts
+++ src/app/components/clientes/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ClienteService } from '../../../services/cliente.service';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +6,5 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email: FormControl = new FormControl(''); 
 
-  constructor(private clienteService: ClienteService) { }
-
-  onSubmit() {
-    const emailValue = this.email.value; 
-    this.clienteService.obteneterCliente(emailValue)
-      .then(data => {
-        console.log(data); 
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
 }
